8a4f73831e75a89b3becb4ea84338c934efaba91
Matheus Cezario <mcezario11@gmail.com>
2018-11-02 10:29:25 -0300

    Knight

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishpo;
 import chess.pieces.King;
+import chess.pieces.Knight;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
@@ -193,6 +195,10 @@ public class ChessMatch {
 		 placeNewPiece('f',2, new Pawn(board, Color.WHITE));
 		 placeNewPiece('g',2, new Pawn(board, Color.WHITE));
 		 placeNewPiece('h',2, new Pawn(board, Color.WHITE));
+		 placeNewPiece('f',1, new Bishpo(board, Color.WHITE));
+		 placeNewPiece('c',1, new Bishpo(board, Color.WHITE));
+		 placeNewPiece('b',1, new Knight(board, Color.WHITE));
+		 placeNewPiece('g',1, new Knight(board, Color.WHITE));
 		 
 		 placeNewPiece('a',8, new Rook(board, Color.BLACK));
 		 placeNewPiece('h',8, new Rook(board, Color.BLACK));
@@ -205,6 +211,10 @@ public class ChessMatch {
 		 placeNewPiece('f',7, new Pawn(board, Color.BLACK));
 		 placeNewPiece('g',7, new Pawn(board, Color.BLACK));
 		 placeNewPiece('h',7, new Pawn(board, Color.BLACK));
+		 placeNewPiece('f',8, new Bishpo(board, Color.BLACK));
+		 placeNewPiece('c',8, new Bishpo(board, Color.BLACK));
+		 placeNewPiece('b',8, new Knight(board, Color.BLACK));
+		 placeNewPiece('g',8, new Knight(board, Color.BLACK));
 	}
 	
 	
